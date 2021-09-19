f8ad81e08987f1f153c4e3d29bd00340415d4d71
SangeethaKumarasamy <vpksangeetha@gmail.com>
2021-09-19 11:39:03 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var obj_resume={
+   
+    basics:{
+    name:"Sangeetha Kumarsamy",
+    email:"vpksangeetha@gmail.com",
+    mobile_number:"824892932",
+    Address:"Sankari,Salem",
+    pincode:"637301",
+    },
+    education:{
+        pg_qualification:"ME.,Communication Systems",
+        college:"Kongu Engineering college,Erode",
+        Passed_out_year:"2018",
+        CGPA:"7.5",
+        ug_qualification:"BE.,ECE",
+        college:"Vivekanandha college of Engineering for women,Namakkal",
+        Passed_out_year:"2016",
+        CGPA:"71.2", 
+    },
+    work_experience:{
+        company:"CSIR-National Physical Laboratory",
+        location:"New Delhi",
+        Position:"student-Intern",
+        period:"6_months(1st dec,2017 to 5th june 2018)",
+        project_name:"A study on the influence of various parameters of antenna in TWSTFT",        
+    },
+    publications:{
+        ME_phaseI_project:"poster publication",
+        location:"Chennai",
+        year:"2017",
+        Theme:"Digital India",
+        Event_name:"IISF",
+    },
+    software:{
+        Name:"Ansys(Antenna design)",
+    },
+   }
+ 
+console.log(obj_resume);
